5239984f4ecfbf90e68c3e948acdd7a205d5fc09
Nick Overdijk <nick@astrant.net>
2013-04-11 22:39:44 +0200

    Adds append for std::set

--- algorithm.hpp
+++ algorithm.hpp
@@ -2,6 +2,7 @@
 
 #include <cstdlib>
 #include <map>
+#include <set>
 #include <algorithm>
 #include <type_traits>
 
@@ -106,9 +107,21 @@ auto append(Container1& x, Container2 const& y) -> decltype(x) {
 	return x;
 }
 
-template <typename Container, typename Element>
+template <typename Container2, typename T>
+auto append(std::set<T>& x, Container2 const& y) -> decltype(x) {
+	x.insert(std::begin(y), std::end(y));
+	return x;
+}
+
+template <typename Element, typename Container>
 auto append(Container& x, std::initializer_list<Element>&& y) -> decltype(x) {
 	x.insert(x.end(), y.begin(), y.end());
+	return x;
+}
+
+template <typename Element>
+auto append(std::set<Element>& x, std::initializer_list<Element>&& y) -> decltype(x) {
+	x.insert(y.begin(), y.end());
 	return x;
 }
 
