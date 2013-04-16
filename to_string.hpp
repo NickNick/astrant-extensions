bfb602b5114c9e9a7102941f908eaf4dc7dc731c
Dennis Brentjes <d.brentjes@gmail.com>
2013-04-16 16:20:44 +0200

    Slight change in mingw compatibility
    If you include the to_string header and the macros
    _WIN32 and _GLIBCXX_HAVE_BROKEN_VSWPRINTF are defined this header
    now puts the astrant::to_string in the std namespace.

--- to_string.hpp
+++ to_string.hpp
@@ -14,3 +14,14 @@ namespace astrant {
 		return s.str();
 	}
 }
+
+/* The standard mingw distribution reports a broken VSWPRINTF and thus has no 
+ * std::to_string. In these cases we use a replacement to_string and put it in 
+ * the std namespace. The mingw64 on the other hand has a working implementation
+ * this is why we check for the existance of win32 and this flag.
+ */
+#if defined ( _WIN32 ) && _GLIBCXX_HAVE_BROKEN_VSWPRINTF == 1
+namespace std {
+	using astrant::to_string;
+}
+#endif
