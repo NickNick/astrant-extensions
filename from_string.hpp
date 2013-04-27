bb44c082401e45f8505a55c5205c6fae9a190ca1
Nick Overdijk <nick@astrant.net>
2013-04-27 23:28:40 +0200

    Add specialisations to from_string for float, (unsigned) int

--- from_string.hpp
+++ from_string.hpp
@@ -21,6 +21,21 @@ namespace astrant {
 	}
 
 	template <>
+	inline float from_string<float>(std::string const& x){
+		return std::stof(x);
+	}
+
+	template <>
+	inline int from_string<int>(std::string const& x){
+		return std::stoi(x);
+	}
+
+	template <>
+	inline unsigned int from_string<unsigned int>(std::string const& x){
+		return std::stoul(x);
+	}
+
+	template <>
 	inline std::string from_string<std::string>(std::string const& x){
 		return x;
 	}
