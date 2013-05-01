8a69626f7c2576c4ff8190ee5afb8a8c936429a7
Nick Overdijk <nick@astrant.net>
2013-05-01 17:27:54 +0200

    Add astrant::max, returns maximum value from a container

--- algorithm.hpp
+++ algorithm.hpp
@@ -140,4 +140,19 @@ auto flatten(T const& x) -> rcrr<decltype(*std::begin(x))> {
 	return y;
 }
 
+template <typename Container>
+auto max(Container const& c) -> typename Container::value_type {
+	using Element = typename Container::value_type;
+
+	Element max = std::numeric_limits<Element>::max();
+
+	for(auto const& e : c){
+		if (max < e){
+			max = e;
+		}
+	}
+
+	return max;
+}
+
 }
