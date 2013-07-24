20f320cdaa0f8f92b6ce74901f8fd0d344a5de29
Nick Overdijk <nick@astrant.net>
2013-07-24 20:48:48 +0200

    Removes VCOUT, VCERR, renames COUT, CERR to ACOUT, ACERR

--- stream.hpp
+++ stream.hpp
@@ -3,8 +3,5 @@
 #include <iostream>
 
 #define INFORMATION __FILE__ << ":" << __LINE__ << "(" << __FUNCTION__ << ")"
-#define COUT std::cout << INFORMATION << "\n"
-#define CERR std::cerr << INFORMATION << "\n"
-
-#define VCOUT if(verbose) COUT
-#define VCERR if(verbose) CERR
+#define ACOUT std::cout << INFORMATION << "\n"
+#define ACERR std::cerr << INFORMATION << "\n"
