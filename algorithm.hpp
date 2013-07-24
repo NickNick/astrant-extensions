c4fdd1b745ee60bf260bf4345b5cae6112c6ab37
Nick Overdijk <nick@astrant.net>
2013-07-24 20:49:04 +0200

    Adds astrant::product(Container)

--- algorithm.hpp
+++ algorithm.hpp
@@ -5,6 +5,7 @@
 #include <set>
 #include <algorithm>
 #include <type_traits>
+#include <array>
 
 namespace astrant {
 //! Maps @param value to a range of [0, 1]
@@ -159,6 +160,20 @@ auto max(Container const& c) -> typename Container::value_type {
 	}
 
 	return max;
+}
+
+template <typename T, size_t size>
+T product(std::array<T, size> const& x){
+	static_assert(size > 0, "Size must be > 0 to get the product from this array");
+	//TODO: Enable when supported by the compiler (gcc 4.6.3 on ubuntu 12.04 doesn't support this yet)
+	//static_assert(std::is_copy_constructible<T>, "T must be copy-constructible");
+
+	T return_value = x[0];
+	for(size_t i = 1; i < size; ++i){
+		return_value *= x[i];
+	}
+
+	return return_value;
 }
 
 }
