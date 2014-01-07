fa05a18b900fd774345a4dbccf3bd1662cffff42
Joshua Moerman <lakseru@gmail.com>
2014-01-07 23:15:17 +0100

    [remove_elements] fixes &-bug

--- algorithm.hpp
+++ algorithm.hpp
@@ -34,14 +34,14 @@ T random_value(T const& min, T const& max){
 
 /*! Sorted insert using std::lower_bound */
 template <typename Container>
-typename Container::iterator sorted_insert(Container c, typename Container::value_type x){
+typename Container::iterator sorted_insert(Container & c, typename Container::value_type x){
 	typename Container::iterator it = std::lower_bound(c.begin(), c.end(), x);
 	return c.insert(it, x);
 }
 
 /*! Sorted insert using std::lower_bound with comparator */
 template <typename Container, typename Comparator>
-typename Container::iterator sorted_insert(Container c, typename Container::value_type x, Comparator comp){
+typename Container::iterator sorted_insert(Container & c, typename Container::value_type x, Comparator comp){
 	typename Container::iterator it = std::lower_bound(c.begin(), c.end(), x, comp);
 	return c.insert(it, x);
 }
