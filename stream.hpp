8d5baeb01c6f1e40059e92e806b69c32a85ec8b6
Nick Overdijk <nick@astrant.net>
2014-03-05 05:49:14 +0100

    Rewrote stream.hpp, deprecates COUT, CERR, INFORMATION, VCOUT, VCERR

--- stream.hpp
+++ stream.hpp
@@ -1,16 +1,20 @@
 #pragma once
 
 #include <iostream>
+#include <iomanip>
 
-#define INFORMATION (std::string(__FILE__) + ":" + std::to_string(__LINE__) + "(" + __FUNCTION__ + ")")
-#define COUT std::cout << INFORMATION << "\n"
-#define CERR std::cerr << INFORMATION << "\n"
+#define ASTRANT_STRING_CURRENT_PLACE_INFORMATION (std::string(__FILE__) + ":" + std::to_string(__LINE__) + "(" + __FUNCTION__ + ")")
+#define ASTRANT_STREAM_CURRENT_PLACE_INFORMATION (std::string(__FILE__) + ":") << std::setw(4) << std::to_string(__LINE__) << "(" <<  std::setw(20) << __FUNCTION__ << ")"
+#define ACOUT  std::cout << ASTRANT_STREAM_CURRENT_PLACE_INFORMATION  << "\n"
+#define ASDOUT std::cout << ASTRANT_STREAM_CURRENT_PLACE_INFORMATION  << ": "
+#define ACERR  std::cerr << ASTRANT_STREAM_CURRENT_PLACE_INFORMATION  << "\n"
+#define ASDERR std::cerr << ASTRANT_STREAM_CURRENT_PLACE_INFORMATION  << ": "
 
-#define ACOUT COUT
-#define ACERR CERR
-
-#define ASDOUT std::cout << INFORMATION << ": "
-#define ASDERR std::cerr << INFORMATION << ": "
-
+//! @{
+//! @deprecated
+#define COUT ACOUT
+#define CERR ACERR
+#define INFORMATION ASTRANT_STRING_CURRENT_PLACE_INFORMATION
 #define VCOUT if(verbose) COUT
 #define VCERR if(verbose) CERR
+//! @}
