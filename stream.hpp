aac7153966308c77bd7a5e19728ff85264f80c4c
Nick Overdijk <nick@astrant.net>
2014-04-17 10:29:19 +0200

    Shortens the filepath a bit in stream.hpp

--- stream.hpp
+++ stream.hpp
@@ -2,9 +2,21 @@
 
 #include <iostream>
 #include <iomanip>
+#include <string>
 
-#define ASTRANT_STRING_CURRENT_PLACE_INFORMATION (std::string(__FILE__) + ":" + std::to_string(__LINE__) + "(" + __FUNCTION__ + ")")
-#define ASTRANT_STREAM_CURRENT_PLACE_INFORMATION (std::string(__FILE__) + ":") << std::setw(4) << std::to_string(__LINE__) << "(" <<  std::setw(20) << __FUNCTION__ << ")"
+namespace astrant {
+	inline std::string cut_left_if_too_large(std::string s, size_t size){
+		if(s.size() > size){
+			return s.substr(s.size() - size);
+		} else {
+			return s;
+		}
+	}
+}
+
+#define ASTRANT_STRING_CURRENT_PLACE_INFORMATION (astrant::cut_left_if_too_large(__FILE__, 20) + ":" + std::to_string(__LINE__) + "(" + __FUNCTION__ + ")")
+#define ASTRANT_STREAM_CURRENT_PLACE_INFORMATION (astrant::cut_left_if_too_large(__FILE__, 20) + ":") << std::setw(4) << std::to_string(__LINE__) << "(" <<  std::setw(15) << __FUNCTION__ << ")"
+
 #define ACOUT  std::cout << ASTRANT_STREAM_CURRENT_PLACE_INFORMATION  << "\n"
 #define ASDOUT std::cout << ASTRANT_STREAM_CURRENT_PLACE_INFORMATION  << ": "
 #define ACERR  std::cerr << ASTRANT_STREAM_CURRENT_PLACE_INFORMATION  << "\n"
