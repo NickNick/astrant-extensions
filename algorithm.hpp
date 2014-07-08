3804a7e2081334f7615e54bba297c8c3ffe0dd26
Joshua Moerman <lakseru@gmail.com>
2014-07-08 21:56:05 +0200

    Adds prepend as counterpart of append

--- algorithm.hpp
+++ algorithm.hpp
@@ -135,13 +135,25 @@ auto append(std::set<T>& x, Container2 const& y) -> decltype(x) {
 
 template <typename Container>
 auto append(Container& x, std::initializer_list<typename Container::value_type>&& y) -> decltype(x) {
-	x.insert(x.end(), y.begin(), y.end());
+	x.insert(std::end(x), std::begin(y), std::end(y));
 	return x;
 }
 
 template <typename Element>
 auto append(std::set<Element>& x, std::initializer_list<Element>&& y) -> decltype(x) {
-	x.insert(y.begin(), y.end());
+	x.insert(std::begin(y), std::end(y));
+	return x;
+}
+
+template <typename Container1, typename Container2>
+auto prepend(Container1& x, Container2 const& y) -> decltype(x) {
+	x.insert(std::begin(x), std::begin(y), std::end(y));
+	return x;
+}
+
+template <typename Container>
+auto prepend(Container& x, std::initializer_list<typename Container::value_type>&& y) -> decltype(x) {
+	x.insert(std::begin(x), std::begin(y), std::end(y));
 	return x;
 }
 
