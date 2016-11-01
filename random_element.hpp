1b3a68437c533ed42e783a38c23d1c603ee78b0b
Nick Overdijk <nick@astrant.net>
2016-11-01 07:02:26 +0100

    Fix includes and make sure b & e are of the same type.

--- random_element.hpp
+++ random_element.hpp
@@ -1,22 +1,32 @@
 #pragma once
 
+#include "random_value.hpp"
 #include <initializer_list>
 
 namespace astrant {
 
 template <typename Container>
 auto random_element(Container const& c) -> decltype(c[0]){
-	return c[random_value(0u, c.size())];
+	auto e = c.size();
+	auto b = e;
+	b = 0;
+	return c[random_value(b, e)];
 }
 
 template <typename Container>
 auto random_element(Container& c) -> decltype(c[0]){
-	return c[random_value(0u, c.size())];
+	auto e = c.size();
+	auto b = e;
+	b = 0;
+	return c[random_value(b, e)];
 }
 
 template <typename Element>
 auto random_element(std::initializer_list<Element>&& c) -> decltype(*c.begin()){
-	auto const it = c.begin() + random_value(size_t(0), c.size());
+	auto e = c.size();
+	auto b = e;
+	b = 0;
+	auto const it = c.begin() + random_value(b, e);
 	return *it;
 }
 
